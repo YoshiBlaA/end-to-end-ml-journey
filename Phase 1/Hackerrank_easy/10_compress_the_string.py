25f4985bcf8fea1ea9f190ca61227ab62513809f
YoshiBla <74738048+YoshiBlaA@users.noreply.github.com>
2026-05-06 01:07:49 -0600

    Delete Phase 1/Hackerrank_easy directory

--- Phase 1/Hackerrank_easy/10_compress_the_string.py
+++
@@ -1,8 +0,0 @@
-#https://www.hackerrank.com/challenges/compress-the-string/problem
-
-import itertools
-
-data = list(input())
-
-for character_key, group in itertools.groupby(data):
-    print(f"({len(list(group))}, {character_key})", end=" ")
